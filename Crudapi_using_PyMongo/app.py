97e3ea7e0afdecb27fd357748c8a4926a8edec9b
AfridaRSamma <afrida.r.samma@gmail.com>
2020-10-10 20:48:27 +0600

    added db in compose.yml

--- Crudapi_using_PyMongo/app.py
+++ Crudapi_using_PyMongo/app.py
@@ -1,87 +1,98 @@
-from flask import Flask
-from flask_pymongo import PyMongo
+from flask import Flask,Response
+import pymongo
+import json
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 from flask import jsonify,request
 from werkzeug.security import generate_password_hash,check_password_hash
 
 app=Flask(__name__)
-#app.secret_key='secretkey'
-app.config['MONGO_URI']="mongodb://db_for_crudapp:27017"
-
-mongo=PyMongo(app)
-
-@app.route('/add',methods=['POST'])
-def add_user():
-    json=request.json
-    name=json['name']
-    email=json['email']
-    password=json['pwd']
-
-    if name and email and password and request.method=='POST':
-        hashed_password=generate_password_hash(password)
-        id = mongo.db.user.insert({'name':name,'email':email,'pwd':hashed_password})
-        resp=jsonify('user added successfully')
-        resp.status_code=200
-        return resp
-
-    else :
-        return not_found()
-
-@app.errorhandler(404)
-def not_found(error=None):
-    message={
-        'status':404,
-        'message':'NOT FOUND'+ request.url
-    }
-
-    resp=jsonify(message)
-    resp.status_code = 404
-    return resp
-
-@app.route('/users')
-def users():
-    user=mongo.db.user.find()
-    resp=dumps(users)
-    return resp
-
-@app.route("/user/<id>")
-def user(id):
-    user = mongo.db.user.find_one({'id':ObjectId(id)})
-    resp=dumps(user)
-    return resp
-
-@app.route('/delete/<id>',methods=['DELETE'])
-def delete_user(id):
-    mongo.db.user.delete_one({"id":ObjectId(id)})
-    resp=jsonify('user deleted successfully')
-    resp.status_code =200
-    return resp
-
-@app.route('/update/<id>',methods=['PUT'])
-def update_user(id):
-    id=id
-    json=request.json
-    name=json['name']
-    email=json['email']
-    password=json['pwd']
-    
-    if name and email and password and request.method=='PUT':
-        hashed_password=generate_password_hash(password)
-        mongo.db.user.update_one({'id':ObjectId(id['$oid']) if '$oid' in id else ObjectId(id)},{'$set':{'name':name,'email':email,'pwd':hashed_password}})
-        resp=jsonify('user updated successfully')
-        resp.status_code=200
-        return resp
-
-    else :
-        return not_found()
 
 @app.route('/')
 def ping_server():
-    return "Welcome to the world of data science."
+    return "Welcome to the world of mine."
 
+#try:
+default_connection_url = "mongodb+srv://sam:01521@cluster0.vycaj.mongodb.net/test1?retryWrites=true&w=majority"
+db_name = 'Category'
+
+# create connection
+client = pymongo.MongoClient(default_connection_url)
+
+# create database
+database = client[db_name]
+
+# create collection
+collection_name = 'Employee'
+collection=database[collection_name]
+
+# initially inserted  data
+# record={'name': 'iNeuron','description': 'Affordable AI'}         
+# collection.insert_one(record)
+
+print(client.list_database_names())
+
+#except:
+#    print("Error - Cannot connect to db")
+
+
+try:
+    @app.route('/add',methods=['POST'])
+    def add_employee():
+        json=request.json
+        name=json['name']
+        description=json['description']
+
+        if name and description and request.method=='POST':            
+            record_add={'name':name,'description':description}
+            collection.insert_one(record_add)            
+            resp=jsonify('user added successfully'),200
+            return resp
+
+        else :
+            return 'not_found add_employee'
+except:
+    print("could not find /add route")
+
+
+try:
+    @app.route('/showall')
+    def show_all_employee():
+        all_record= collection.find()
+        record_show=[{"name": rec["name"], "description": rec["description"]} for rec in all_record]
+        resp=jsonify('showing data',record_show),200
+        return resp
+except:
+    print("could not find /showall route")
+
+
+try:
+    @app.route('/delete/<id>',methods=['DELETE'])
+    def delete_employee(id):
+        collection.delete_one({"_id":ObjectId(id)})
+        return Response(
+            response=json.dumps(
+               {"message":"Deleted employee","id":f"{id}"}))
+except:
+    print("could not find /delete route")
+
+
+try:
+    @app.route('/update/<id>',methods=['PATCH'])
+    def update_employee(id):
+        collection.update_one(
+            {"_id":ObjectId(id)},
+            {"$set":{"name":request.form["name"],"description":request.form['description']}}
+            )
+
+        # for attr in dir(dbResponse):
+        #  print(f"********{attr}*******")
+        return Response(
+            response=json.dumps(
+                {"message":"Updated employee","id":f"{id}"}))
+except:
+    print("could not find update route ")
+    
     
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
-
-
