d8594b31600746b0c16c0583cf721465f3bc90ff
AfridaRSamma <afrida.r.samma@gmail.com>
2020-10-15 10:58:59 +0600

    added db config initial

---
+++ Crudapi_using_PyMongo_customDB/boot.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+printf "\n"
+printf "****** Creating Network ******\n"
+docker network create flask-mongo-network;
+printf "****** Network Created ******\n"
+
+printf "\n"
+
+printf "****** Starting db_mongo Container ******\n"
+docker container run \
+    --detach \
+    --name=db_mongo \
+    --env MONGO_INITDB_ROOT_USERNAME=m_user \
+    --env MONGO_INITDB_ROOT_PASSWORD=m_pass \
+    --env MONGO_INITDB_DATABASE=mydb \
+    --net flask-mongo-network \
+    mongo;
+printf "****** db_mongo Container Started ******\n"
+
+printf "\n"
+
+printf "****** Creating api_flask Image ******\n"
+docker image build . --tag api_flask;
+printf "****** api_flask Image Created ******\n"
+
+printf "\n" 
+
+printf "****** Starting api_flask Container ******\n"
+docker container run \
+    --detach \
+    --name=api_flask \
+    --env-file .env \
+    --publish=8010:5000 \
+    --net flask-mongo-network \
+    api_flask;
+printf "****** api_flask Container Started ******\n"
+
+printf "\n"
+
+printf "****** All Containers are Up and Running ******"
+
+printf "\n"
